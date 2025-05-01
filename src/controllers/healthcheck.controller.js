06fc44dcbb4c944b8c152390098eee445dc53867
bhanupratapsingh21 <bhanupss137@gmail.com>
2025-05-01 20:54:10 +0530

    added health check routes

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -11,26 +11,13 @@ const healthcheck = asynchandler(async (req, res) => {
         const totallike = await like.countDocuments({});
         const totalComments = await Comment.countDocuments({});
 
-        // Aggregate total views from all videos
-        const totalViewsResult = await Video.aggregate([
-            {
-                $group: {
-                    _id: null,
-                    totalViews: { $sum: "$views" }
-                }
-            }
-        ]);
-
-        const totalViews = totalViewsResult.length > 0 ? totalViewsResult[0].totalViews : 0;
 
         return res.status(200).json(new ApiResponse(200, {
             Stateofwholeapp: {
                 totalComments,
-                totalUser,
                 totallike,
-                totalViews
             },
-        }, "Health :: All System Is Good And Thanks To Hitesh Sir For This Series #ChaiaurCode"));
+        }, "Health :: All System Are Healthy"));
     } catch (error) {
         console.error('Error in health check:', error);
         return res.status(500).json(new ApiError(500, {}, "Internal Server Error"));
